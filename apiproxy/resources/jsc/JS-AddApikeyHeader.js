760a8781b543ed7ba7777e401f14f29b93bc2880
eceledonio <eceledonio@nearbpo.com>
2021-04-26 12:55:25 -0500

    x-apikey header is required

--- apiproxy/resources/jsc/JS-AddApikeyHeader.js
+++ apiproxy/resources/jsc/JS-AddApikeyHeader.js
@@ -1,5 +1,6 @@
 try {
-    var apiKey="LfZX1oMw1Q2xQgwrQFnY6IzBBcpwpVa1BIm1L8ED";
+    var apiKey = context.getVariable("request.header.x-apikey");
+    //var apiKey="LfZX1oMw1Q2xQgwrQFnY6IzBBcpwpVa1BIm1L8ED";
     var url = "https://api.nasa.gov/planetary/apod";
     context.setVariable("target.url", url + "?api_key=" + apiKey);
 } catch (error) {
